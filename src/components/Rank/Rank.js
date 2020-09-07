24ce3e021a65b96031f885ea7d4dad1bc014a047
Antarpreet singh <antarpreetsingh566@gmail.com>
2020-09-08 00:59:06 +0530

    deploy my facerecognition app

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+const Rank = () =>{
+    return (
+        <div >
+            <div className='white f3'>
+                {'By:- Antarpreet Singh'}
+
+            </div>
+            <div className='white f1'>
+                {'Face Recognition App'}
+                
+
+            </div>
+            
+        </div>
+
+    );
+}
+
+export default Rank ;
