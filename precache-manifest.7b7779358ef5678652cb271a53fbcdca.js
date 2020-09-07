55203cf2134ddfac26f793823a05a42d637a3eaa
Antarpreet singh <antarpreetsingh566@gmail.com>
2020-09-08 01:00:14 +0530

    Updates

---
+++ precache-manifest.7b7779358ef5678652cb271a53fbcdca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5edc3a461a10f3a6e4167b301c3a931b",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "fc8df90b02cd36accc39",
+    "url": "/FaceRecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a9352ac32ed363ef798a",
+    "url": "/FaceRecognition/static/css/main.4fd9f0d2.chunk.css"
+  },
+  {
+    "revision": "fc8df90b02cd36accc39",
+    "url": "/FaceRecognition/static/js/2.4ad10584.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FaceRecognition/static/js/2.4ad10584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9352ac32ed363ef798a",
+    "url": "/FaceRecognition/static/js/main.e9414d0b.chunk.js"
+  },
+  {
+    "revision": "fe111d7312de8cc63f32",
+    "url": "/FaceRecognition/static/js/runtime-main.70cbb673.js"
+  },
+  {
+    "revision": "6258c5828e7257bab4a170a9ad7bf5c6",
+    "url": "/FaceRecognition/static/media/brain1.6258c582.png"
+  }
+]);
